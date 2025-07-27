3af88f126eccd97e780148fc392068469ce5885c
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2025-07-27 15:20:46 +0800

    config

--- coworker_bingo/bingo_sheet_generator.py
+++ coworker_bingo/bingo_sheet_generator.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+
+
 class BingoSheetGenerator:
+    @dataclass
+    class Config:
+        participant: str
+        participants: set[str]
+        specific_facts: dict[str, list[str]]
+        generic_facts: list[str]
+
     def __init__(self) -> None:
         pass
